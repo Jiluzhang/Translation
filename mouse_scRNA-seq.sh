51ef6e36c97322f878776a3b66f9f5b3de72a929
Luz <779410241@qq.com>
2023-09-14 17:29:09 +0800

    Update mouse_scRNA-seq.sh

--- mouse_scRNA-seq.sh
+++ mouse_scRNA-seq.sh
@@ -3,9 +3,10 @@ tar -xvf GSE132184_RAW.tar
 
 for GSM in GSM3852665 GSM3852666 GSM3852667 GSM3852668;do
 	tar -xzvf `echo $GSM\*filtered_feature_bc_matrix.tar.gz`
-	gunzip barcodes.tsv.gz features.tsv.gz
-	mkdir /fs/DC/mouse/raw_mat_meta/GSE132/GSE132184/$GSM
-	mv barcodes.tsv features.tsv matrix.mtx.gz /fs/DC/mouse/raw_mat_meta/GSE132/GSE132184/$GSM
+	mkdir -p /fs/DC/mouse/raw_mat_meta/GSE132/GSE132184/$GSM
+	mv barcodes.tsv.gz features.tsv.gz matrix.mtx.gz /fs/DC/mouse/raw_mat_meta/GSE132/GSE132184/$GSM
 	rm `echo $GSM\*filtered_feature_bc_matrix.tar.gz`
 	echo $GSM done
 done
+
+meta data???
