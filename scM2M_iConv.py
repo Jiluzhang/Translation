d4f14281d335dbca90876bd63e08cffe809c672c
Luz <779410241@qq.com>
2024-03-14 17:21:23 +0800

    Update scM2M_iConv.py

--- scM2M_iConv.py
+++ scM2M_iConv.py
@@ -1,3 +1,3 @@
 # /data/home/zouqihang/desktop/project/M2M/version0.4.2
 
-accelerate launch --config_file accelerator_config.yaml rna2atac_pretrain.py -d . -n test --batch_size 24 > logs/arg1_running_logs.txt
+accelerate launch --config_file accelerator_config.yaml rna2atac_pretrain.py -d /fs/home/jiluzhang/scM2M_iConv/data -n test --batch_size 24 > logs/arg1_running_logs.txt
