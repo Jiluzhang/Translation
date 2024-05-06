0a14818a51c7df9ae434148bc0cfcd9a2a944a6b
Luz <779410241@qq.com>
2024-05-06 17:46:34 +0800

    Update scM2M_final.sh

--- scM2M_final.sh
+++ scM2M_final.sh
@@ -1,1 +1,7 @@
 ## rna2atac_config.yaml  rna2atac_data_preprocess.py  rna2atac_evaluate.py  rna2atac_pretrain.py
+
+# pan_cancer_atac_dataset_0.h5ad  head 1000
+# pan_cancer_rna_dataset_0.h5ad   head 1000
+
+python rna2atac_data_preprocess.py  # ~6.5 min
+accelerate launch --config_file accelerator_config.yaml rna2atac_pretrain.py -d ./preprocessed_data -n rna2atac_test
