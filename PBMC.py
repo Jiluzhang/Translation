3e5e9054312d79ad20febe027f6a364bdc4b02bd
Luz <779410241@qq.com>
2024-06-19 21:45:36 +0800

    Update PBMC.py

--- PBMC.py
+++ PBMC.py
@@ -731,11 +731,11 @@ accelerate launch --config_file accelerator_config.yaml --main_process_port 2982
 
 accelerate launch --config_file accelerator_config.yaml --main_process_port 29822 rna2atac_evaluate.py \
                   -d ./preprocessed_data_val \
-                  -l save/2024-06-16_rna2atac_train_29/pytorch_model.bin --config_file rna2atac_config_val_eval.yaml
+                  -l save/2024-06-19_rna2atac_train_60/pytorch_model.bin --config_file rna2atac_config_val_eval.yaml
 
 accelerate launch --config_file accelerator_config.yaml --main_process_port 29822 rna2atac_evaluate.py \
                   -d ./preprocessed_data_test_2048 \
-                  -l save/2024-06-18_rna2atac_train_40/pytorch_model.bin --config_file rna2atac_config_val_eval_2.yaml
+                  -l save/2024-06-19_rna2atac_train_60/pytorch_model.bin --config_file rna2atac_config_val_eval_2.yaml
 
 mv predict.npy test_predict.npy
 
@@ -1207,34 +1207,7 @@ for i in model_1.state_dict():
 
 
 
-############ BMMC dataset ############
-# wget -c https://ftp.ncbi.nlm.nih.gov/geo/series/GSE194nnn/GSE194122/suppl/GSE194122_openproblems_neurips2021_multiome_BMMC_processed.h5ad.gz
-import scanpy as sc
-bmmc = sc.read_h5ad('GSE194122_openproblems_neurips2021_multiome_BMMC_processed.h5ad')
-bmmc.obs.cell_type.value_counts()
-# cell_type
-# CD8+ T                 11589
-# CD14+ Mono             10843
-# NK                      6929
-# CD4+ T activated        5526
-# Naive CD20+ B           5052
-# Erythroblast            4916
-# CD4+ T naive            4398
-# Transitional B          2810
-# Proerythroblast         2300
-# CD16+ Mono              1894
-# B1 B                    1890
-# Normoblast              1780
-# Lymph prog              1779
-# G/M prog                1203
-# pDC                     1191
-# HSC                     1072
-# CD8+ T naive            1012
-# MK/E prog                884
-# cDC2                     859
-# ILC                      835
-# Plasma cell              379
-# ID2-hi myeloid prog      108
+
 
 
 
