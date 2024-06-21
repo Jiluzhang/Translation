255af001cccfa5146233093e7c306ba3c32507ff
Luz <779410241@qq.com>
2024-06-22 02:00:32 +0800

    Update CL.py

--- CL.py
+++ CL.py
@@ -1,4 +1,5 @@
 ## cell line dataset
+## reference: hg19 !!!!!!!!!!!!!!!!!!
 
 # wget -c https://static-content.springer.com/esm/art%3A10.1038%2Fs41467-018-08205-7/MediaObjects/41467_2018_8205_MOESM6_ESM.xls   # ATAC
 # wget -c https://static-content.springer.com/esm/art%3A10.1038%2Fs41467-018-08205-7/MediaObjects/41467_2018_8205_MOESM7_ESM.xls   # RNA
