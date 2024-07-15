4d5b07cac2d9051d5465af9ae8a173c8c6d267b4
Luz <779410241@qq.com>
2024-07-15 14:18:11 +0800

    Create Mouse_embryo.py

---
+++ Mouse_embryo.py
@@ -0,0 +1,4 @@
+## Decoding gene regulation in the mouse embryo using single-cell multi-omics
+## https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE205117
+## workdir: /fs/home/jiluzhang/scM2M_no_dec_attn/mouse_embryo
+nohup wget -c https://ftp.ncbi.nlm.nih.gov/geo/series/GSE205nnn/GSE205117/suppl/GSE205117_RAW.tar > 20240715_download.log &
