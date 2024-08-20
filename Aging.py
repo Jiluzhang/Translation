c886a30950d3da37e6e3b855e174c523bc212f2a
Luz <779410241@qq.com>
2024-08-20 17:28:46 +0800

    Update Aging.py

--- Aging.py
+++ Aging.py
@@ -3,9 +3,23 @@
 ##                            M_Kidney_Chromium_Nuc_Isolation_vs_SaltyEZ_vs_ComplexTissueDP_filtered_feature_bc_matrix.h5
 ## kidney.h5
 
-dat = sc.read_10x_h5('kidney.h5', gex_only=False)           # 14652 × 97637
-atac = dat[:, dat.var['feature_types']=='Peaks']            # 14652 × 65352
-rna = dat[:, dat.var['feature_types']=='Gene Expression']   # 14652 × 32285
+import scanpy as sc
+dat = sc.read_10x_h5('kidney.h5', gex_only=False)                  # 14652 × 97637
+dat.var_names_make_unique()
+
+rna = dat[:, dat.var['feature_types']=='Gene Expression'].copy()   # 14652 × 32285
+rna.write('rna.h5ad')
+
+atac = dat[:, dat.var['feature_types']=='Peaks'].copy()            # 14652 × 65352
+atac.write('atac.h5ad')
+
+
+python split_train_val.py --RNA rna.h5ad --ATAC atac.h5ad --train_pct 0.9
+python data_preprocess.py -r rna_train.h5ad -a atac_train.h5ad -s preprocessed_data_train --dt train --config rna2atac_config_train.yaml
+python data_preprocess.py -r rna_val.h5ad -a atac_val.h5ad -s preprocessed_data_val --dt val --config rna2atac_config_val.yaml
+nohup accelerate launch --config_file accelerator_config_train.yaml --main_process_port 29823 rna2atac_train.py --config_file rna2atac_config_train.yaml \
+                        --train_data_dir ./preprocessed_data_train --val_data_dir ./preprocessed_data_val -n rna2atac_train > 20240820.log &   # 3304881
+
 
 
 ## Tabula Muris Senis: https://cellxgene.cziscience.com/collections/0b9d8a04-bb9d-44da-aa27-705bb65b54eb
