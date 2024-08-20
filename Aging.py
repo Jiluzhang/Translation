97db4c7baf1c25c72a2c6798157105d58b0ee3fd
Luz <779410241@qq.com>
2024-08-20 23:02:13 +0800

    Update Aging.py

--- Aging.py
+++ Aging.py
@@ -4,6 +4,7 @@
 ## kidney.h5
 
 import scanpy as sc
+
 dat = sc.read_10x_h5('kidney.h5', gex_only=False)                  # 14652 × 97637
 dat.var_names_make_unique()
 
@@ -11,6 +12,7 @@ rna = dat[:, dat.var['feature_types']=='Gene Expression'].copy()   # 14652 × 32
 rna.write('rna.h5ad')
 
 atac = dat[:, dat.var['feature_types']=='Peaks'].copy()            # 14652 × 65352
+atac.X[atac.X!=0] = 1
 atac.write('atac.h5ad')
 
 
@@ -18,11 +20,54 @@ python split_train_val.py --RNA rna.h5ad --ATAC atac.h5ad --train_pct 0.9
 python data_preprocess.py -r rna_train.h5ad -a atac_train.h5ad -s preprocessed_data_train --dt train --config rna2atac_config_train.yaml
 python data_preprocess.py -r rna_val.h5ad -a atac_val.h5ad -s preprocessed_data_val --dt val --config rna2atac_config_val.yaml
 nohup accelerate launch --config_file accelerator_config_train.yaml --main_process_port 29823 rna2atac_train.py --config_file rna2atac_config_train.yaml \
-                        --train_data_dir ./preprocessed_data_train --val_data_dir ./preprocessed_data_val -n rna2atac_train > 20240820.log &   # 3304881
+                        --train_data_dir ./preprocessed_data_train --val_data_dir ./preprocessed_data_val -n rna2atac_train > 20240820.log &   # 42406
 
 
 
 ## Tabula Muris Senis: https://cellxgene.cziscience.com/collections/0b9d8a04-bb9d-44da-aa27-705bb65b54eb
 # kidney dataset: wget -c https://datasets.cellxgene.cziscience.com/e896f2b3-e22b-42c0-81e8-9dd1b6e80d64.h5ad
 # -> kidney_aging.h5ad
-dat_all = sc.read_h5ad('kidney_aging.h5ad')
+
+import scanpy as sc
+
+dat = sc.read_h5ad('kidney_aging.h5ad')
+
+## map rna
+ref_rna = sc.read_h5ad('rna.h5ad')
+genes = ref_rna.var[['gene_ids']]
+genes['gene_name'] = genes.index.values
+genes.reset_index(drop=True, inplace=True)
+
+genes = pd.read_table('human_genes.txt', names=['gene_ids', 'gene_name'])
+
+rna_exp = pd.concat([rna.var, pd.DataFrame(rna.X.T, index=rna.var.index.values)], axis=1)
+X_new = pd.merge(genes, rna_exp, how='left', on='gene_ids').iloc[:, 3:].T
+X_new.fillna(value=0, inplace=True)
+
+rna_new = sc.AnnData(X_new.values, obs=rna.obs, var=pd.DataFrame({'gene_ids': genes['gene_ids'], 'feature_types': 'Gene Expression'}))
+rna_new.var.index = genes['gene_name'].values
+rna_new.X = csr_matrix(rna_new.X)
+rna_new.write('rna.h5ad')
+
+
+
+ref_atac = sc.read_h5ad('atac.h5ad')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
