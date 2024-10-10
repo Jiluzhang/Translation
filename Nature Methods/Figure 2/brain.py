f861a896c291165545de70e746ccb242b21a66d3
Luz <779410241@qq.com>
2024-10-10 10:59:25 +0800

    Update brain.py

--- Nature Methods/Figure 2/brain.py
+++ Nature Methods/Figure 2/brain.py
@@ -1,1 +1,11 @@
 #### brain datasets from scenario_4
+import scanpy as sc
+import snapatac2 as snap
+import seaborn as sns
+
+true = sc.read_h5ad('atac_test.h5ad')
+snap.pp.select_features(true)
+snap.tl.spectral(true)
+snap.tl.umap(true)
+sc.pl.umap(true, color=['chr17:44915696-44915866'], legend_fontsize='7', legend_loc='right margin', size=5,
+           title='', frameon=True, save='_true.pdf')
