8152cc03674a13a81fee7923a68b6a3d6f912ff6
Luz <779410241@qq.com>
2025-01-16 18:04:21 +0800

    Create Spatial_science.py

---
+++ Nature Methods/Spatial_science.py
@@ -0,0 +1,11 @@
+#### https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi
+#### Single-cell genomics and regulatory networks for 388 human brains
+
+## RT00372N
+## wget -c https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8155nnn/GSM8155483/suppl/GSM8155483_RT00372N_RNA_barcodes.txt.gz
+## wget -c https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8155nnn/GSM8155483/suppl/GSM8155483_RT00372N_RNA_matrix.mtx.gz
+## wget -c https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8155nnn/GSM8155503/suppl/GSM8155503_RT00372N_ATAC_barcodes.txt.gz
+## wget -c https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8155nnn/GSM8155503/suppl/GSM8155503_RT00372N_ATAC_matrix.mtx.gz
+## wget -c https://brainscope.gersteinlab.org/data/snrna_expr_matrices_zip/Girgenti-multiome.zip
+
+## need to find raw count data!!!!!!!!!!
