90f191b57af1a1511167a99c3ea65fb579ad82e9
Luz <779410241@qq.com>
2025-01-18 12:17:10 +0800

    Update Spatial_science.py

--- Nature Methods/Spatial_science.py
+++ Nature Methods/Spatial_science.py
@@ -97,15 +97,123 @@ nohup accelerate launch --config_file accelerator_config_train.yaml --main_proce
                         --train_data_dir train_pt --val_data_dir val_pt -s save -n rna2atac_brain > 20250116.log &   # 1163370
 
 
+######################################################## filter & map rna & atac h5ad ########################################################
+import scanpy as sc
+import numpy as np
+import pandas as pd
+from scipy.sparse import csr_matrix
+
+rna = sc.read_h5ad('spatial_rna.h5ad')  #  2500 × 38244
+
+sc.pp.filter_genes(rna, min_cells=10)       # 2500 × 13997
+# sc.pp.filter_cells(rna, min_genes=200)      # 2196 × 13997  (do not filter cells)
+# sc.pp.filter_cells(rna, max_genes=20000)    # 2196 × 13997
+np.count_nonzero(rna.X.toarray(), axis=1).max()   # 6589
+
+atac = sc.read_h5ad('spatial_atac.h5ad')  #  2500 × 1033239
+
+sc.pp.filter_genes(atac, min_cells=10)       # 2500 × 657953
+# sc.pp.filter_cells(atac, min_genes=500)      # 2429 × 657953
+# sc.pp.filter_cells(atac, max_genes=50000)    # 2424 × 657953
+np.count_nonzero(atac.X.toarray(), axis=1).max()   # 60239
+
+# idx = np.intersect1d(rna.obs.index, atac.obs.index)
+# del rna.var['n_cells']
+# rna[idx, :].copy().write('spatial_rna_filtered.h5ad')     # 2500 × 13997
+# del atac.var['n_cells']
+# atac[idx, :].copy().write('spatial_atac_filtered.h5ad')   # 2500 × 657953
+
+# rna = rna[idx, :].copy()
+# atac = atac[idx, :].copy()
+
+rna_ref = sc.read_h5ad('./rna.h5ad')   # 8369 × 17532
+genes = rna_ref.var[['gene_ids']].copy()
+genes['gene_name'] = genes.index.values
+genes.reset_index(drop=True, inplace=True)
+rna_exp = pd.concat([rna.var, pd.DataFrame(rna.X.toarray().T, index=rna.var.index)], axis=1)
+X_new = pd.merge(genes, rna_exp, how='left', on='gene_ids').iloc[:, 4:].T
+X_new.fillna(value=0, inplace=True)
+rna_new = sc.AnnData(X_new.values, obs=rna.obs, var=pd.DataFrame({'gene_ids': genes['gene_ids'], 'feature_types': 'Gene Expression'})) 
+# rna_new = sc.AnnData(X_new.values, obs=rna.obs[['cell_anno']], var=pd.DataFrame({'gene_ids': genes['gene_ids'], 'feature_types': 'Gene Expression'}))
+rna_new.var.index = genes['gene_name'].values
+rna_new.X = csr_matrix(rna_new.X)   # 2500 × 17532
+rna_new.write('spatial_rna_test.h5ad')
+np.count_nonzero(rna_new.X.toarray(), axis=1).max()  # 6378
+
+## atac
+atac_ref = sc.read_h5ad('./atac.h5ad')   # 8369 × 404378
+
+peaks = atac_ref.var[['gene_ids']].copy()
+peaks['gene_name'] = peaks.index.values
+peaks.reset_index(drop=True, inplace=True)
+atac_exp = pd.concat([atac.var, pd.DataFrame(atac.X.toarray().T, index=atac.var.index)], axis=1)
+X_new = pd.merge(peaks, atac_exp, how='left', on='gene_ids').iloc[:, 4:].T
+X_new.fillna(value=0, inplace=True)
+atac_new = sc.AnnData(X_new.values, obs=atac.obs, var=pd.DataFrame({'gene_ids': peaks['gene_ids'], 'feature_types': 'Peaks'}))
+# atac_new = sc.AnnData(X_new.values, obs=atac.obs[['cell_anno']], var=pd.DataFrame({'gene_ids': peaks['gene_ids'], 'feature_types': 'Peaks'}))
+atac_new.var.index = peaks['gene_name'].values
+atac_new.X = csr_matrix(atac_new.X)   # 2500 × 404378
+atac_new.write('spatial_atac_test.h5ad')
+########################################################################################################################################################################
+
+
+#### clustering for true with rna
+import scanpy as sc
+
+rna = sc.read_h5ad('spatial_rna_test.h5ad')
+sc.pp.normalize_total(rna, target_sum=1e4)
+sc.pp.log1p(rna)
+sc.pp.highly_variable_genes(rna)
+rna = rna[:, rna.var.highly_variable]
+sc.tl.pca(rna)
+sc.pp.neighbors(rna)
+sc.tl.umap(rna)
+sc.tl.leiden(rna, resolution=0.9)
+rna.obs['leiden'].value_counts()
+# 0    688
+# 1    424
+# 2    400
+# 3    301
+# 4    299
+# 5    174
+# 6    152
+# 7     62
+
+rna.obs['cell_anno'] = rna.obs['leiden']
+rna.write('rna_test.h5ad')
+
+atac = sc.read_h5ad('spatial_atac_test.h5ad')
+atac.obs['cell_anno'] = rna.obs['leiden']
+atac.write('atac_test.h5ad')
+
+
+
+python data_preprocess.py -r spatial_rna_test.h5ad -a spatial_atac_test.h5ad -s spatial_test_pt --dt test --config rna2atac_config_test.yaml
+accelerate launch --config_file accelerator_config_test.yaml --main_process_port 29822 rna2atac_predict.py \
+                  -d ./spatial_test_pt -l ./save/2025-01-17_rna2atac_brain_5/pytorch_model.bin --config_file rna2atac_config_test.yaml
+python npy2h5ad.py
+python cal_auroc_auprc.py --pred atac_cisformer.h5ad --true atac_test.h5ad
+# Cell-wise AUROC: 0.6449
+# Cell-wise AUPRC: 0.0229
+# Peak-wise AUROC: 0.5162
+# Peak-wise AUPRC: 0.0134
+python plot_save_umap.py --pred atac_cisformer.h5ad --true atac_test.h5ad
+python cal_cluster.py --file atac_cisformer_umap.h5ad
+# AMI: 0.3397
+# ARI: 0.2487
+# HOM: 0.3527
+# NMI: 0.3432
+
+
 #### scbutterfly
-nohup python scbt_b.py > scbt_b_20250117.log &   # 1201918
+nohup python scbt_b.py > scbt_b_20250117.log &   # 2391368
 
 # cp predict/R2A.h5ad atac_scbt.h5ad
 # python cal_auroc_auprc.py --pred atac_scbt.h5ad --true atac_test.h5ad
 # python plot_save_umap.py --pred atac_scbt.h5ad --true atac_test.h5ad
 # python cal_cluster.py --file atac_scbt_umap.h5ad
 
-# nohup python scbt_b_predict.py > scbt_b_predict.log &  # 3580964
+nohup python scbt_b_predict.py > scbt_b_predict.log &  # 2382519
 # cp predict/R2A.h5ad atac_scbt.h5ad
 # python cal_auroc_auprc.py --pred atac_scbt.h5ad --true atac_test.h5ad
 # python plot_save_umap.py --pred atac_scbt.h5ad --true atac_test.h5ad
@@ -124,13 +232,20 @@ nohup python /fs/home/jiluzhang/BABEL/bin/train_model.py --data train_val.h5 --o
 # python plot_save_umap.py --pred atac_babel.h5ad --true atac_test.h5ad
 # python cal_cluster.py --file atac_babel_umap.h5ad
 
-# python h5ad2h5.py -n test
-# nohup python /fs/home/jiluzhang/BABEL/bin/predict_model.py --checkpoint ../train_out --data test.h5 --outdir test_out --device 1 --nofilter --noplot --transonly > predict_20241226.log &
-# # 3593000
-# python match_cell.py
-# python cal_auroc_auprc.py --pred atac_babel.h5ad --true atac_test.h5ad
-# python plot_save_umap.py --pred atac_babel.h5ad --true atac_test.h5ad
-# python cal_cluster.py --file atac_babel_umap.h5ad
-
+python h5ad2h5.py -n test
+nohup python /fs/home/jiluzhang/BABEL/bin/predict_model.py --checkpoint ./train_out --data test.h5 --outdir test_out --device 3 --nofilter --noplot --transonly > predict_20250118.log &
+# 2398520
+python match_cell.py
+python cal_auroc_auprc.py --pred atac_babel.h5ad --true atac_test.h5ad
+# Cell-wise AUROC: 0.6839
+# Cell-wise AUPRC: 0.0257
+# Peak-wise AUROC: 0.6408
+# Peak-wise AUPRC: 0.026
+python plot_save_umap.py --pred atac_babel.h5ad --true atac_test.h5ad
+python cal_cluster.py --file atac_babel_umap.h5ad
+# AMI: 0.2982
+# ARI: 0.1813
+# HOM: 0.3041
+# NMI: 0.3016
 
 
