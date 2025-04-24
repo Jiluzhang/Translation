940fc5152d79b9cfd6cc4acf3c7660951b1da955
Luz <779410241@qq.com>
2025-04-24 11:05:14 +0800

    Update atac2rna.py

--- Nature Methods/Figure 2/atac2rna.py
+++ Nature Methods/Figure 2/atac2rna.py
@@ -173,7 +173,7 @@ endo_spe['tf'].values
 # 'ZNF385D', 'TIGD1', 'FBXL19', 'HINFP', 'LIN28A', 'LIN28B', 'YY1', 'ZNF610', 'PHF1', 'SP4', 'ETV3L', 'YY2', 'SPDEF', 'GSC', 'ZNF501',
 # 'ARNTL', 'CREB3', 'ZBTB37', 'ETV2', 'ELF1', 'SIX5', 'ELF4', 'EHF', 'MLX', 'TFEC', 'USF1', 'ATF4', 'ETV6', 'ELK1', 'TFEB', 'MITF',
 # 'SALL1', 'ZNF619'
-[ETV2, ELK1, ELF1, ELF4] & [TFEB, MITF, ARNTL, YY1, SPDEF, ATF4, TFEC]
+# [ETV2, ELK1, ELF1, ELF4] & [TFEB, MITF, ARNTL, YY1, SPDEF, ATF4, TFEC]
 
 df_spe = pd.concat([t_cell_spe, b_cell_spe, macro_spe, fibro_spe, endo_spe])
 df_spe.to_csv('motifs_enrich_luz.txt', sep='\t', index=False)
@@ -276,96 +276,48 @@ df = pd.DataFrame({'tf':naive['name'],
                    'ex_log2fc':ex['log2(fold change)'],
                    'ex_qval':ex['adjusted p-value']})
 
-########################################################################
-########################################################################
-########################################################################
-########################################################################
-########################################################################
-########################################################################
+naive_spe = df[(df['naive_log2fc']>np.log2(1.2)) & (df['naive_qval']<0.05) &
+               (df['naive_log2fc']>df['effect_log2fc']) &
+               (df['naive_log2fc']>df['ex_log2fc'])]
+naive_spe = naive_spe.sort_values('naive_log2fc', ascending=False)
+
+effect_spe = df[(df['effect_log2fc']>np.log2(1.2)) & (df['effect_qval']<0.05) &
+                (df['effect_log2fc']>df['naive_log2fc']) &
+                (df['effect_log2fc']>df['ex_log2fc'])]
+effect_spe = effect_spe.sort_values('effect_log2fc', ascending=False)
 
 ex_spe = df[(df['ex_log2fc']>np.log2(1.2)) & (df['ex_qval']<0.05) &
-                (df['t_cell_log2fc']>(df['b_cell_log2fc']+0.1)) &
-                (df['t_cell_log2fc']>(df['macro_log2fc']+0.1)) &
-                (df['t_cell_log2fc']>(df['fibro_log2fc']+0.1)) &
-                (df['t_cell_log2fc']>(df['endo_log2fc']+0.1))]   # 38
+            (df['ex_log2fc']>df['naive_log2fc']) &
+            (df['ex_log2fc']>df['effect_log2fc'])]
 ex_spe = ex_spe.sort_values('ex_log2fc', ascending=False)
-b_cell_spe = df[(df['b_cell_log2fc']>np.log2(1.2)) & (df['b_cell_qval']<0.05) &
-                (df['b_cell_log2fc']>(df['t_cell_log2fc']+0.1)) &
-                (df['b_cell_log2fc']>(df['macro_log2fc']+0.1)) &
-                (df['b_cell_log2fc']>(df['fibro_log2fc']+0.1)) &
-                (df['b_cell_log2fc']>(df['endo_log2fc']+0.1))]   # 14
-b_cell_spe = b_cell_spe.sort_values('b_cell_log2fc', ascending=False)
-macro_spe = df[(df['macro_log2fc']>np.log2(1.2)) & (df['macro_qval']<0.05) &
-                (df['macro_log2fc']>(df['t_cell_log2fc']+0.1)) &
-                (df['macro_log2fc']>(df['b_cell_log2fc']+0.1)) &
-                (df['macro_log2fc']>(df['fibro_log2fc']+0.1)) &
-                (df['macro_log2fc']>(df['endo_log2fc']+0.1))]   # 29
-macro_spe = macro_spe.sort_values('macro_log2fc', ascending=False)
-fibro_spe = df[(df['fibro_log2fc']>np.log2(1.2)) & (df['fibro_qval']<0.05) &
-                (df['fibro_log2fc']>(df['t_cell_log2fc']+0.1)) &
-                (df['fibro_log2fc']>(df['b_cell_log2fc']+0.1)) &
-                (df['fibro_log2fc']>(df['macro_log2fc']+0.1)) &
-                (df['fibro_log2fc']>(df['endo_log2fc']+0.1))]   # 27
-fibro_spe = fibro_spe.sort_values('fibro_log2fc', ascending=False)
-endo_spe = df[(df['endo_log2fc']>np.log2(1.2)) & (df['endo_qval']<0.05) &
-                (df['endo_log2fc']>(df['t_cell_log2fc']+0.4)) &
-                (df['endo_log2fc']>(df['b_cell_log2fc']+0.4)) &
-                (df['endo_log2fc']>(df['macro_log2fc']+0.4)) &
-                (df['endo_log2fc']>(df['fibro_log2fc']+0.4))]   # 33
-endo_spe = endo_spe.sort_values('endo_log2fc', ascending=False)
 
-t_cell_spe['tf'].values
-# 'HSF5', 'DMRT3', 'DMRT1', 'ZNF653', 'PBX4', 'DMRT2', 'VSX2', 'BCL6', 'HESX1', 'HSFY1', 'SEBOX', 'GSX1', 'SHOX', 'PRRX2',
-# 'PRRX1', 'ALX3', 'ISX', 'LBX1', 'NKX2-3', 'POU4F2', 'DMRTA1', 'MLXIP', 'DBX1', 'TBX2', 'LIN54', 'SOX7', 'TEAD2', 'FOS', 'NFE2L1',
-# 'TEAD4', 'NFE2L2', 'ZNF324', 'SIX2', 'BACH1', 'NFE2', 'SIX1', 'TEAD1', 'HLX'
-# [BCL6, FOS, NFE2L1, NFE2L2]
+naive_spe['tf'].values
+# 'MECOM', 'PRDM16', 'ZBTB45', 'PAX3', 'HMGA2', 'ZBTB32', 'MEF2B', 'TBX21', 'MEIS2', 'ZNF433', 'ZNF285', 'TGIF2LY', 'MEOX1', 'PKNOX1',
+# 'FOXA3', 'NFIL3', 'ZNF716', 'SOX7', 'NKX2-1', 'LEF1', 'ARID5B', 'SOX21', 'TGIF2', 'OTX1', 'TBX15', 'ZNF250', 'MYF6', 'OTX2',
+# 'FOXL2', 'FOXD2', 'FOXB2', 'ZNF274', 'ZNF264', 'ZNF236', 'ZFP82','NFIA', 'MEIS3', 'NFIX'
+# [LEF1]
 
-b_cell_spe['tf'].values
-# 'ETV3', 'E2F5', 'PROX1', 'PROX2', 'ZNF454', 'FLI1', 'RUNX2', 'ZNF343', 'RUNX3', 'ZNF213', 'ZBTB11', 'GLI2', 'THAP1', 'ZNF816'
-# [RUNX1, RUNX3, FLI1, PROX1]
+effect_spe['tf'].values
+# 'GMEB1', 'NRF1', 'ZSCAN10', 'ATF7', 'AC023509.3', 'YY2', 'CREB1', 'YY1', 'ZBED1', 'OVOL1', 'ETV4', 'ZBTB33', 'HINFP', 'FOSB',
+# 'ZNF385D', 'FEV', 'ATF2', 'GABPA', 'ERF', 'FOSL1', 'ZNF200', 'CREB3', 'ELK4', 'OVOL2', 'NFYC', 'CREBL2', 'E4F1', 'ZNF597',
+# 'KLF14', 'JDP2', 'LIN28A', 'LIN28B', 'TCFL5', 'ATF1', 'SP4', 'ELF1', 'ZNF441', 'ETV3L', 'SP1', 'SP9', 'EGR1', 'ETV1', 'NFYA',
+# 'TIGD1', 'MTF2', 'KLF15', 'CREM', 'KLF7', 'TIGD2', 'JRKL', 'SP8', 'E2F5', 'AHR', 'HES4', 'BATF3', 'ZNF100', 'SP5', 'KMT2A', 'DNMT1',
+# 'MBD2', 'ZNF501', 'ZNF541', 'ZNF783', 'ETV3', 'KLF13', 'KLF12'
+[BATF3, CREB1, YY1, EGR1]
 
-macro_spe['tf'].values
-# 'SMAD3', 'BHLHE22', 'OLIG1', 'BBX', 'NEUROG2', 'DMRTB1', 'BARX2', 'MTERF1', 'POU2AF1', 'ZNF154', 'EOMES', 'NFE2L3', 'FOXD4L5',
-# 'FOXD4L6', 'FOXD4L4', 'FOXD4L3', 'FOXD4L1', 'FOXD4', 'TBX3', 'HOXC4', 'FERD3L', 'ZBTB3', 'STAT5A', 'ZNF329', 'TCF7L2', 'FOXR1',
-# 'HAND2', 'FOXA2', 'ZNF224'
-# [SMAD3, STAT5A, TCF7L2, FOXA2]
+ex_spe['tf'].values
+# 'NEUROG1', 'NEUROG2', 'CEBPG', 'SOX1', 'DPF1', 'NFE2', 'HSFY1', 'NEUROG3', 'NFE2L1', 'FOS', 'DUX1', 'PROP1', 'MEF2A',
+# 'FOXO6', 'HOXD4', 'HOXB4', 'SOX14', 'FOSL2', 'BACH1', 'IRX3', 'JUNB', 'NFATC2', 'ZNF177', 'SOX15', 'TBX1', 'POU6F1', 'CDC5L',
+# 'ZNF382', 'FOXR2', 'GSX1', 'HNF1B', 'POU4F1', 'DNTTIP1', 'NFE2L2', 'MIXL1', 'FOXD1', 'DRGX', 'LHX3', 'BACH2', 'POU3F2', 'FOXO1',
+# 'ZNF35', 'ZNF45', 'LMX1B', 'MAFK', 'POU5F1B', 'DUX3', 'POU2F2', 'NFAT5', 'CEBPA', 'ZNF157', 'ZNF490', 'ZNF260'
+[NFATC2, NFE2L2]
 
-fibro_spe['tf'].values
-# 'SIX6', 'GCM2', 'PROP1', 'POU3F4', 'POU3F1', 'ZNF655', 'NKX3-1', 'HBP1', 'TBP', 'GFI1B', 'SOX6', 'SIX3', 'IRX4', 'ZNF296', 'TGIF1',
-# 'FOXR2', 'IRF9', 'ZFP82', 'ZNF35', 'FOXQ1', 'ZFP69', 'IRF8', 'IRF1', 'SMAD4', 'CPEB1', 'STAT4', 'SOX11'
-# [SMAD4, STAT4, IRF1, SOX11]
-
-endo_spe['tf'].values
-# 'ZNF385D', 'TIGD1', 'FBXL19', 'HINFP', 'LIN28A', 'LIN28B', 'YY1', 'ZNF610', 'PHF1', 'SP4', 'ETV3L', 'YY2', 'SPDEF', 'GSC', 'ZNF501',
-# 'ARNTL', 'CREB3', 'ZBTB37', 'ETV2', 'ELF1', 'SIX5', 'ELF4', 'EHF', 'MLX', 'TFEC', 'USF1', 'ATF4', 'ETV6', 'ELK1', 'TFEB', 'MITF',
-# 'SALL1', 'ZNF619'
-[ETV2, ELK1, ELF1, ELF4] & [TFEB, MITF, ARNTL, YY1, SPDEF, ATF4, TFEC]
-
-df_spe = pd.concat([t_cell_spe, b_cell_spe, macro_spe, fibro_spe, endo_spe])
-df_spe.to_csv('motifs_enrich_luz.txt', sep='\t', index=False)
-
-## plot heatmap for enriched TFs in pan-cancer
-import pandas as pd
-import numpy as np
-from plotnine import *
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-plt.rcParams['pdf.fonttype'] = 42
-
-df_raw = pd.read_table('motifs_enrich_luz.txt')
-df_raw.index = df_raw['tf'].values
-df_raw = df_raw[df_raw['tf']!='TIGD1']  # remove inf
-df = df_raw[['t_cell_log2fc', 'b_cell_log2fc', 'macro_log2fc', 'fibro_log2fc', 'endo_log2fc']]
-
-sns.clustermap(df.T, cmap='coolwarm', row_cluster=False, col_cluster=False, z_score=1, vmin=-1.5, vmax=1.5, figsize=(40, 8))  # standard_scale=1, z_score=1
-plt.savefig('motifs_enrich_luz_heatmap.pdf')
-plt.close()
-
+df_spe = pd.concat([naive_spe, effect_spe, ex_spe])
+df_spe.to_csv('t_cell_motifs_enrich_luz.txt', sep='\t', index=False)
 
 ## fibro
 fibro = pd.read_table('fibro_specific_enhancers_attention_raw_data.tsv', header=None)
-fibro.index = fibro.iloc[:, 0].values
+fibro.index = fibro[0].values
 fibro = fibro.iloc[:, 1:]
 fibro_ref = pd.read_table('fibro_specific_enhancers_accessibility_raw_data.tsv', index_col=0)
 fibro.columns = fibro_ref.columns
@@ -374,16 +326,89 @@ sns.heatmap(fibro, cmap='coolwarm', vmin=-1, vmax=1, xticklabels=False)
 plt.savefig('fibro_specific_enhancers.png')
 plt.close()
 
-## macro
-macro = pd.read_table('macro_specific_enhancers_attention_raw_data.tsv', header=None)
-macro.index = macro.iloc[:, 0].values
-macro = macro.iloc[:, 1:]
-macro_ref = pd.read_table('macro_specific_enhancers_accessibility_raw_data.tsv', index_col=0)
-macro.columns = macro_ref.columns
+saa1_peaks = fibro.columns[fibro.loc['qFibro_SAA1']==1].values          # 70901
+cthrc1_peaks = fibro.columns[fibro.loc['eFibro_CTHRC1']==1].values      # 50929
+col11a1_peaks = fibro.columns[fibro.loc['eFibro_COL11A1']==1].values    # 14392
+ccl5_peaks = fibro.columns[fibro.loc['apFibro_CCL5']==1].values         # 35405
+myh11_peaks = fibro.columns[fibro.loc['MyoFibro_MYH11']==1].values      # 46977
 
-sns.heatmap(macro, cmap='coolwarm', vmin=-1, vmax=1, xticklabels=False)
-plt.savefig('macro_specific_enhancers.png')
+peaks_dict = {'saa1': saa1_peaks, 'cthrc1': cthrc1_peaks, 'col11a1': col11a1_peaks,
+              'ccl5': ccl5_peaks, 'myh11': myh11_peaks}
+motifs_enrich = snap.tl.motif_enrichment(motifs=snap.datasets.cis_bp(unique=True),
+                                         regions=peaks_dict,
+                                         genome_fasta=snap.genome.hg38,
+                                         background=None,
+                                         method='hypergeometric')
+with open('fibro_motifs_enrich_luz.pkl', 'wb') as file:
+    pickle.dump(motifs_enrich, file)
+
+# with open('fibro_motifs_enrich_luz.pkl', 'rb') as file:
+#     motifs_enrich = pickle.load(file)
+
+saa1 = motifs_enrich['saa1'].to_pandas()
+cthrc1 = motifs_enrich['cthrc1'].to_pandas()
+col11a1 = motifs_enrich['col11a1'].to_pandas()
+ccl5 = motifs_enrich['ccl5'].to_pandas()
+myh11 = motifs_enrich['myh11'].to_pandas()
+
+df = pd.DataFrame({'tf':saa1['name'],
+                   'saa1_log2fc':saa1['log2(fold change)'], 'saa1_qval':saa1['adjusted p-value'],
+                   'cthrc1_log2fc':cthrc1['log2(fold change)'], 'cthrc1_qval':cthrc1['adjusted p-value'],
+                   'colla1_log2fc':colla1['log2(fold change)'], 'colla1_qval':colla1['adjusted p-value'],
+                   'ccl5_log2fc':ccl5['log2(fold change)'], 'ccl5_qval':ccl5['adjusted p-value'],
+                   'myh11_log2fc':myh11['log2(fold change)'], 'myh11_qval':myh11['adjusted p-value'],})
+
+saa1_spe = df[(df['saa1_log2fc']>np.log2(1.2)) & (df['saa1_qval']<0.05)]
+saa1_spe = saa1_spe.sort_values('saa1_log2fc', ascending=False)
+
+cthrc1_spe = df[(df['cthrc1_log2fc']>np.log2(1.2)) & (df['cthrc1_qval']<0.05)]
+cthrc1_spe = cthrc1_spe.sort_values('cthrc1_log2fc', ascending=False)
+
+col11a1_spe = df[(df['col11a1_log2fc']>np.log2(1.2)) & (df['col11a1_qval']<0.05)]
+col11a1_spe = col11a1_spe.sort_values('col11a1_log2fc', ascending=False)
+
+ccl5_spe = df[(df['ccl5_log2fc']>np.log2(1.2)) & (df['ccl5_qval']<0.05)]
+ccl5_spe = ccl5_spe.sort_values('ccl5_log2fc', ascending=False)
+
+myh11_spe = df[(df['myh11_log2fc']>np.log2(1.2)) & (df['myh11_qval']<0.05)]
+myh11_spe = myh11_spe.sort_values('myh11_log2fc', ascending=False)
+
+saa1_spe['tf'].values
+cthrc1_spe['tf'].values
+col11a1_spe['tf'].values
+ccl5_spe['tf'].values
+myh11_spe['tf'].values
+
+
+
+
+
+
+
+
+df_spe = pd.concat([naive_spe, effect_spe, ex_spe])
+df_spe.to_csv('t_cell_motifs_enrich_luz.txt', sep='\t', index=False)
+
+
+## plot heatmap for enriched TFs in t cell
+import pandas as pd
+import numpy as np
+from plotnine import *
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.rcParams['pdf.fonttype'] = 42
+
+df_raw = pd.read_table('t_cell_motifs_enrich_luz.txt')
+df_raw.index = df_raw['tf'].values
+df = df_raw[['naive_log2fc', 'effect_log2fc', 'ex_log2fc']]
+
+sns.clustermap(df.T, cmap='coolwarm', row_cluster=False, col_cluster=False, z_score=1, vmin=-1.5, vmax=1.5, figsize=(40, 8))  # standard_scale=1, z_score=1
+plt.savefig('t_cell_motifs_enrich_luz_heatmap.pdf')
 plt.close()
+
+
+
 
 
 
