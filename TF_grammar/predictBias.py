ab4cd2ffbf23cdad1c1dd3d9a84ad33381a95e19
Luz <779410241@qq.com>
2025-11-03 19:46:24 +0800

    Update predictBias.py

--- TF_grammar/predictBias.py
+++ TF_grammar/predictBias.py
@@ -1,9 +1,47 @@
-# import warnings
-# warnings.filterwarnings('ignore')
+############################ convert bigwig to csv ############################
+## python bw2csv.py  # ~4 min
 
-# import os
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import pyfaidx
+import pyBigWig
+import pandas as pd
+import math
+from tqdm import * 
 
+genome = pyfaidx.Fasta("ecoli.fa")
+bw = pyBigWig.open('ecoli_nakedDNA.bw')
+
+t = 0
+context_lst = []
+obsBias_lst = []
+idx_lst = []
+start_lst = []
+
+for i in tqdm(range(50, len(genome['NC_000913.3'][:].seq)-50), ncols=80):
+    signal = bw.values('NC_000913.3', i, i+1)[0]
+    if not math.isnan(signal) and signal!=0:
+        context_lst.append(genome['NC_000913.3'][(i-50):(i+50+1)].seq)
+        obsBias_lst.append(signal)
+        ## 0:training  1:validation  2:testing
+        if i<=3641652:
+            idx_lst.append(0)
+        elif i<=4141651:
+            idx_lst.append(1)
+        else:
+            idx_lst.append(2)
+        start_lst.append(i)
+        t += 1
+
+bw.close()
+
+res = pd.DataFrame({'context':context_lst, 'obsBias':obsBias_lst, 'idx':idx_lst,
+                    'chrom':'NC_000913.3', 'start':start_lst})   # 2289693
+res['end'] = res['start']+1
+res.to_csv('obsBias_cfoot.tsv', sep='\t', index=False)
+########################################################################################
+
+
+######################################################## train CNN model ########################################################
+## python train.py
 import os
 import sys
 import h5py
@@ -28,40 +66,13 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 tf.config.set_logical_device_configuration(tf.config.experimental.list_physical_devices('GPU')[0], 
                                            [tf.config.LogicalDeviceConfiguration(memory_limit=40960)])
 
-
-##################### parameter setting (Luz) #####################
-import argparse
-
-parser = argparse.ArgumentParser(description='PRINT bias model')
-parser.add_argument('--main_dir', type=str, default='.', help='main directory')
-parser.add_argument('--context_radius', type=int, default=50, help='context radius')
-parser.add_argument('--n_jobs', type=int, default=5, help='core number')
-parser.add_argument('--chunk_size', type=int, default=5, help='chunk size')
-
-args = parser.parse_args()
-main_dir = args.main_dir
-context_radius = args.context_radius
-n_jobs = args.n_jobs
-chunk_size = args.chunk_size
-#################################################################
-
-main_dir = '.'
-context_radius = 50
-n_jobs = 10
-data_dir = '.'
-
-
-#########################
-# Functions we will use #
-#########################
+##############################################
+# functions used #
+##############################################
 
 # One-hot encoding of a DNA sequence.
-# Input: 
-# (1) seq: a string of length L consisting of A/C/G/T
-# Returns: 
-# (1) onehot: L-by-4 encoded matrix
 def onehot_encode(seq):
-    mapping = pd.Series(index = ["A", "C", "G", "T"], data = [0, 1, 2, 3])
+    mapping = pd.Series(index=["A", "C", "G", "T"], data=[0, 1, 2, 3])
     bases = [base for base in seq]
     base_inds = mapping[bases]
     onehot = np.zeros((len(bases), 4))
@@ -69,14 +80,7 @@ def onehot_encode(seq):
     return onehot
 
 # Running one-hot encoding along the sequence of a genomic region
-# Starting from the left, encode every consecutive sub-sequence with length = 2 * context_radius + 1
-# with step size of 1 until we reach the other end of the sequence.
-# Input: 
-# (1) region_seq, a string of the DNA sequence in the region of interest. Must be longer than 2 * context_radius + 1
-# (2) context_radius, radius of every sub-sequence
-# Returns:
-# (1) one-hot encoded sequence contexts for each position in the region
-def region_onehot_encode(region_seq, context_radius = 50):
+def region_onehot_encode(region_seq, context_radius=50):
     
     # Calculate length of every local sub-sequence
     context_len = 2 * context_radius + 1
@@ -102,84 +106,29 @@ def region_onehot_encode(region_seq, context_radius = 50):
 
 # Load ground truth Tn5 bias data
 print("Loading ground truth data")
-bias_data = pd.read_csv(main_dir + "/obsBias_cfoot.tsv", sep = "\t")  # 'context' & 'obsBias_all' & 'BACInd'
+bias_data = pd.read_csv('obsBias_cfoot.tsv', sep='\t')  # 'context' & 'obsBias' & 'idx' & 'chrom' & 'start' & 'end'
 
 # One-hot encoding of sequence contexts
 print("One-hot encoding of sequence contexts")
 seqs = bias_data.loc[:, "context"].values
-with mp.Pool(n_jobs) as pool:
-   onehot_seqs = list(tqdm.tqdm(pool.imap(onehot_encode, seqs), total=len(seqs)))
+with mp.Pool(10) as pool:
+   onehot_seqs = list(tqdm.tqdm(pool.imap(onehot_encode, seqs), total=len(seqs)))  # ~2 min
 onehot_seqs = np.array(onehot_seqs)  # (2289693, 101, 4)
 
-# Transform target values to facilitate training
-target = bias_data.loc[:, "obsBias_all"].values
-# target = np.log10(target + 0.01)
-# target = (target / 2) + 0.5
+# Get target values
+target = bias_data.loc[:, "obsBias"].values
 
-# Get the indices of all BAC regions
-mapped_regions = bias_data.loc[:, "BACInd"].values
-# n_regions = len(np.unique(mapped_regions))
+# Get the indices of all regions
+idx = bias_data.loc[:, "idx"].values
 
-# Divide all the BAC regions into training, validation and test
-# This way, data from each region only falls within one of these datasets
-# print("Splitting all data into training/validation/test")
-# np.random.seed(42)
-# region_inds = np.unique(mapped_regions)
-# np.random.shuffle(region_inds)
-# training_region_inds = region_inds[:int(n_regions * 0.8)]
-# val_region_inds = region_inds[int(n_regions * 0.8):int(n_regions * 0.9)]
-# test_region_inds = region_inds[int(n_regions * 0.9):]
+# Split the encoded sequences and target values into training (0), validation (1) and test (1)
+training_data = onehot_seqs[idx==0]
+training_target = target[idx==0]
+val_data = onehot_seqs[idx==1]
+val_target = target[idx==1]
+test_data = onehot_seqs[idx==2]
+test_target = target[idx==2]
 
-# Find the sequence indices of the corresponding regions
-# training_inds = np.array([i for i in range(len(mapped_regions)) if \
-#                  mapped_regions[i] in training_region_inds])
-# val_inds = np.array([i for i in range(len(mapped_regions)) if \
-#             mapped_regions[i] in val_region_inds])
-# test_inds = np.array([i for i in range(len(mapped_regions)) if \
-#              mapped_regions[i] in test_region_inds])
-
-# Split the encoded sequences and target values into training, validation and test
-# training_data = onehot_seqs[training_inds]
-# training_target = target[training_inds]
-# val_data = onehot_seqs[val_inds]
-# val_target = target[val_inds]
-# test_data = onehot_seqs[test_inds]
-# test_target = target[test_inds]
-
-training_data = onehot_seqs[mapped_regions==0]
-training_target = target[mapped_regions==0]
-val_data = onehot_seqs[mapped_regions==1]
-val_target = target[mapped_regions==1]
-test_data = onehot_seqs[mapped_regions==2]
-test_target = target[mapped_regions==2]
-
-# Up-sample intervals with scarce data. 
-# This is to make sure the training is not heavily influenced with sequences with certain ranges of biases
-# print("Up-sampling training data")
-# n_bins = 5
-# bin_width = (max(training_target) - min(training_target)) / n_bins
-# bin_starts = np.arange(n_bins) * bin_width + min(training_target)
-# bin_ends = np.arange(1, n_bins + 1) * bin_width + min(training_target)
-# bin_inds = [(training_target > bin_starts[i]) & (training_target < bin_ends[i]) 
-#         for i in range(n_bins)]
-# bin_n = [sum(bin_ind) for bin_ind in bin_inds]
-# training_inds = np.concatenate([np.random.choice(training_inds[bin_inds[i]], max(bin_n), replace = True)
-#                                for i in range(n_bins)])
-# np.random.shuffle(training_inds)
-# training_data = onehot_seqs[training_inds]
-# training_target = target[training_inds]
-
-# Data augmentation by taking the reverse complement sequence
-# training_reverse_complement = np.flip(np.flip(training_data, axis = 1), axis = 2)
-# training_data = np.concatenate([training_data, training_reverse_complement])
-# training_target = np.concatenate([training_target, training_target])
-# np.shape(training_data)
-
-# Randomly shuffle augmented data
-# inds = np.arange(np.shape(training_data)[0])
-# np.random.shuffle(inds)
-# training_data = training_data[inds]
-# training_target = training_target[inds]
 
 #################################
 # Model definition and training #
@@ -195,20 +144,20 @@ maxpool_2 = MaxPooling1D()(conv_2)
 conv_3 = Conv1D(32, 5, padding = 'same', activation = 'relu', strides = 1)(maxpool_2)
 maxpool_3 = MaxPooling1D()(conv_3)
 flat = Flatten()(maxpool_3)
-fc = Dense(32,activation = "relu")(flat)
-out = Dense(1,activation = "linear")(fc)
-model = Model(inputs=inputs,outputs=out)  
+fc = Dense(32, activation = "relu")(flat)
+out = Dense(1, activation = "linear")(fc)
+model = Model(inputs=inputs, outputs=out)  
 model.summary()
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mse'])
 
 # Model training
 mse = tf.keras.losses.MeanSquaredError()
-min_loss = np.inf #Luz pre_loss = np.inf
+min_loss = np.inf
 patience = 0
 for n_epoch in range(100):
 
     # New training epoch
-    model.fit(training_data, training_target, batch_size=128, epochs = 1, 
+    model.fit(training_data, training_target, batch_size=256, epochs=1, 
               validation_data=(val_data, val_target))  
 
     # Get MSE loss on the valdation set after current epoch
@@ -218,15 +167,6 @@ for n_epoch in range(100):
 
     # Get pred-target correlation on the validation set after current epoch
     print("Pred-target correlation " + str(ss.pearsonr(val_target, val_pred)[0]))
-
-    # If loss on validation set starts to increase, stop training and adopt the previous saved version
-    # if mse_loss > prev_loss:
-    #     break
-    # else:
-    #     prev_loss = mse_loss
-
-    #     # Save current model version
-    #     model.save(main_dir + "/Tn5_NN_model.h5")
     
     # stop training with valid loss not decrease until 10 epoches
     if mse_loss > min_loss:
@@ -237,43 +177,119 @@ for n_epoch in range(100):
         min_loss = mse_loss
         patience = 0
         print('save model')
-        model.save(main_dir + "/Tn5_NN_model.h5")
+        model.save('Tn5_NN_model.h5')
+#####################################################################################################################
+
+
+######################################################## CNN model evaluation ########################################################
+## python test.py
+
+import os
+import tensorflow as tf
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+tf.config.set_logical_device_configuration(tf.config.experimental.list_physical_devices('GPU')[0], 
+                                           [tf.config.LogicalDeviceConfiguration(memory_limit=40960)])
+
+from keras.models import load_model
+import numpy as np
+import pandas as pd
+import multiprocessing as mp
+import tqdm
+import matplotlib.pyplot as plt
+import scipy.stats as ss
+import pyBigWig
+
+plt.rcParams['pdf.fonttype'] = 42
+
+##############################################
+# functions used #
+##############################################
+
+# One-hot encoding of a DNA sequence.
+def onehot_encode(seq):
+    mapping = pd.Series(index=["A", "C", "G", "T"], data=[0, 1, 2, 3])
+    bases = [base for base in seq]
+    base_inds = mapping[bases]
+    onehot = np.zeros((len(bases), 4))
+    onehot[np.arange(len(bases)), base_inds] = 1
+    return onehot
+
+# Running one-hot encoding along the sequence of a genomic region
+def region_onehot_encode(region_seq, context_radius=50):
+    
+    # Calculate length of every local sub-sequence
+    context_len = 2 * context_radius + 1
+    
+    # If region width is L, then region_seq should be a string of length L + 2 * context_len
+    region_width = len(region_seq) - 2 * context_radius
+    
+    if "N" in region_seq:
+        return np.zeros((region_width, 4))
+    else:
+        # First encode the whole region sequence 
+        # This prevents repetitive computing for overlapping sub-sequences
+        region_onehot = np.array(onehot_encode(region_seq))
         
+        # Retrieve encoded sub-sequences by subsetting the larger encoded matrix
+        region_onehot = np.array([region_onehot[i : (i + context_len), :] for i in range(region_width)])
+        
+        return region_onehot
+
+bias_data = pd.read_csv('obsBias_cfoot.tsv', sep='\t')
+bias_test = bias_data[bias_data['idx']==2]
+
+print("One-hot encoding of sequence contexts")
+seqs = bias_test.loc[:, "context"].values
+with mp.Pool(10) as pool:
+   onehot_seqs = list(tqdm.tqdm(pool.imap(onehot_encode, seqs), total=len(seqs)))
+onehot_seqs = np.array(onehot_seqs)  # (249326, 101, 4)
+
+test_target = bias_test.loc[:, "obsBias"].values
+test_data = onehot_seqs
+
 # Load last Tn5 bias model, which should be the currently best model
-model = load_model(main_dir + "/Tn5_NN_model.h5")
+model = load_model('Tn5_NN_model.h5')
 
 # Model evaluation on the test set
 print("Evaluating performance on the test set")
-plt_ind = np.random.choice(np.arange(len(test_data)), 10000)
+plt_ind = np.random.choice(np.arange(len(test_data)), 1000)
 test_pred = np.transpose(model.predict(test_data))[0]
-# test_target_rev = np.power(10, (test_target - 0.5) * 2) - 0.01
-# test_pred_rev = np.power(10, (test_pred - 0.5) * 2) - 0.01
 
 # Plot test results
-# matplotlib.use('Agg')
-plt.figure(figsize=(5, 5), dpi=300)
+plt.figure(figsize=(5, 5), dpi=100)
 plt.scatter(test_target[plt_ind], test_pred[plt_ind], s=1)
 plt.xlim(0-0.01, 0.8+0.01)
 plt.ylim(0-0.01, 0.8+0.01)
 plt.xlabel("Target label")
 plt.ylabel("Target prediction")
 plt.title("Pearson correlation = " + str(ss.pearsonr(test_target, test_pred)[0]))
-print("Pearson correlation = " + str(ss.pearsonr(test_target, test_pred)[0]))
-plt.savefig(main_dir + "/model_testing.pdf")
+plt.savefig('testing_correlation.pdf')
+print('Plot correlation plot done')
 
 ## extract regions from bigwig files
-import pyBigWig
-
 bw = pyBigWig.open("regions_test_pred.bw", "w")
-
 chroms = {"NC_000913.3": 4641652}
 bw.addHeader(list(chroms.items()))
 test_pred_trans = test_pred.astype(np.float64)
-bw.addEntries(chroms=['NC_000913.3']*((bias_data['BACInd']==2).sum()),
-              starts=list(bias_data[bias_data['BACInd']==2]['start'].values),
-              ends=list(bias_data[bias_data['BACInd']==2]['end'].values),
+bw.addEntries(chroms=['NC_000913.3']*(bias_test.shape[0]),
+              starts=list(bias_test['start'].values),
+              ends=list(bias_test['end'].values),
               values=list(test_pred_trans))
 bw.close()
+print('Output bigwig file done')
+#######################################################################################################################################
+
+
+#### !!!!!!!!!!!!! prediction 0.1  &  install deeptools on PRINT env !!!!!!!!!!!!!
+
+
+multiBigwigSummary bins -b regions_test_pred.bw regions_test.bw -o test.npz --outRawCounts test.tab -l pred raw -bs 1 -p 20
+grep -v nan test.tab | sed 1d > test_nonan.tab
+rm test.tab
+/fs/home/jiluzhang/TF_grammar/cnn_bias_model/data/cal_cor --file test_nonan.tab
+
+
 
 
 ###############################################
@@ -360,67 +376,6 @@ os.system("rm -r " + data_dir + "/chunked_bias_pred")
 
 
 
-# conda install bioconda::pyfaidx
-# pip install pybigwig
-
-import pyfaidx
-import pyBigWig
-import pandas as pd
-import math
-from tqdm import * 
-
-genome = pyfaidx.Fasta("ecoli.fa")
-bw = pyBigWig.open('ecoli_nakedDNA.bw')
-
-t = 0
-context_lst = []
-obsBias_all_lst = []
-BACInd_lst = []
-start_lst = []
-
-for i in tqdm(range(50, len(genome['NC_000913.3'][:].seq)-50), ncols=80):
-    signal = bw.values('NC_000913.3', i, i+1)[0]
-    if not math.isnan(signal) and signal!=0:
-        context_lst.append(genome['NC_000913.3'][(i-50):(i+50+1)].seq)
-        obsBias_all_lst.append(signal)
-        ## 0:training  1:validation  2:testing
-        if i<=3641652:
-            BACInd_lst.append(0)
-        elif i<=4141651:
-            BACInd_lst.append(1)
-        else:
-            BACInd_lst.append(2)
-        start_lst.append(i)
-        t += 1
-
-bw.close()
-
-res = pd.DataFrame({'context':context_lst, 'obsBias_all':obsBias_all_lst, 'BACInd':BACInd_lst,
-                    'chrom':'NC_000913.3', 'start':start_lst})   # 2289693
-res['end'] = res['start']+1
-res.to_csv('obsBias_cfoot.tsv', sep='\t', index=False)
-
-
-## extract regions from bigwig files
-import pyBigWig
-import os
-
-bw = pyBigWig.open("test.bw", "w")
-
-chroms = {"NC_000913.3": 4641652}
-bw.addHeader(list(chroms.items()))
-
-bw.addEntries(chroms=list(res['chrom'].values),
-              starts=list(res['start'].values),
-              ends=list(res['end'].values),
-              values=list(res['obsBias_all'].values))
-bw.close()
-
-
-multiBigwigSummary bins -b regions_test_pred.bw regions_test.bw -o test.npz --outRawCounts test.tab -l pred raw -bs 1 -p 20
-grep -v nan test.tab | sed 1d > test_nonan.tab
-rm test.tab
-/fs/home/jiluzhang/TF_grammar/cnn_bias_model/data/cal_cor --file test_nonan.tab
 
 
 
@@ -432,6 +387,35 @@ rm test.tab
 
 
 
+
+# ##################### parameter setting (Luz) #####################
+# import argparse
+
+# parser = argparse.ArgumentParser(description='PRINT bias model')
+# parser.add_argument('--main_dir', type=str, default='.', help='main directory')
+# parser.add_argument('--context_radius', type=int, default=50, help='context radius')
+# parser.add_argument('--n_jobs', type=int, default=5, help='core number')
+# parser.add_argument('--chunk_size', type=int, default=5, help='chunk size')
+
+# args = parser.parse_args()
+# main_dir = args.main_dir
+# context_radius = args.context_radius
+# n_jobs = args.n_jobs
+# chunk_size = args.chunk_size
+# #################################################################
+
+
+# # Data augmentation by taking the reverse complement sequence
+# training_reverse_complement = np.flip(np.flip(training_data, axis=1), axis=2)
+# training_data = np.concatenate([training_data, training_reverse_complement])
+# training_target = np.concatenate([training_target, training_target])
+# np.shape(training_data)
+
+# # Randomly shuffle augmented data
+# inds = np.arange(np.shape(training_data)[0])
+# np.random.shuffle(inds)
+# training_data = training_data[inds]
+# training_target = training_target[inds]
 
 
 
