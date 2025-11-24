0dfd5da847c33edd98b396a04926ace0b5d682d0
Luz <779410241@qq.com>
2025-11-24 14:55:26 +0800

    Update and rename Bias_correction_summary.py to Bias_correction_ACCESS_ATAC_PRINT.sh

--- TF_grammar/Bias_correction_ACCESS_ATAC_PRINT.sh
+++ TF_grammar/Bias_correction_ACCESS_ATAC_PRINT.sh
@@ -2,6 +2,7 @@
 ## train CNN model
 python ../access_atac/cnn_bias_model/train.py --bw_file ecoli_nakedDNA.bw --train_regions regions_train.bed --valid_regions regions_valid.bed \
                                               --ref_fasta ecoli.fa --k 64 --epochs 200 --out_dir . --out_name ecoli_bias --seed 0 --batch_size 512
+
 ## predict ecoli testing bias
 python ../access_atac/cnn_bias_model/predict.py --regions regions_test.bed --ref_fasta ecoli.fa --k 64 --model_path ecoli_bias.pth \
                                                 --chrom_size_file ecoli_chrom.sizes --out_dir . --out_name ecoli_test_pred
@@ -18,9 +19,21 @@ rm ecoli_test_pred_true.tab
 ## predict human bias
 python ../access_atac/cnn_bias_model/predict.py --regions regions_test_human.bed --ref_fasta hg38.fa --k 64 --model_path ecoli_bias.pth \
                                                 --chrom_size_file hg38_chrom.sizes --out_dir . --out_name human_bias
-  
+
 ## correct bias
 python ../access_atac/bias_correction/correct_bias.py --bw_raw HepG2_7.5U.bw --bw_bias human_bias.bw \
-                                                      --bed_file regions_test.bed --extend 0 --window 101 \
-                                                      --out_dir . --out_name ecoli_to_human_naked_dna_corrected \
-                                                      --chrom_size_file ../hg38.chrom.sizes  # ~5 min for chr21
+                                                      --bed_file regions_test_human.bed --extend 0 --window 101 \
+                                                      --out_dir . --chrom_size_file hg38_chrom.sizes
+
+## merge bias_corrected bw files
+python merge_corrected_bw.py
+rm bias_corrected_chr*.bw 
+
+
+############## PRINT ##############
+## generate tsv file (seq & signal)
+python gen_tsv.py  # ~3.5 min
+
+## train model
+
+  
