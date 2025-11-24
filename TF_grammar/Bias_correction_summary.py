e32cc28cd5594f551f67c49e58d085f0de63c6fe
Luz <779410241@qq.com>
2025-11-24 13:56:59 +0800

    Update Bias_correction_summary.py

--- TF_grammar/Bias_correction_summary.py
+++ TF_grammar/Bias_correction_summary.py
@@ -14,3 +14,13 @@ rm ecoli_test_pred_true.tab
 ./cal_cor --file ecoli_test_pred_true_nonan.tab
 # Pearson R: 0.8809306932571868
 # Spearman R: 0.863951765993428
+
+## predict human bias
+python ../access_atac/cnn_bias_model/predict.py --regions regions_test_human.bed --ref_fasta hg38.fa --k 64 --model_path ecoli_bias.pth \
+                                                --chrom_size_file hg38_chrom.sizes --out_dir . --out_name human_bias
+  
+## correct bias
+python ../access_atac/bias_correction/correct_bias.py --bw_raw HepG2_7.5U.bw --bw_bias human_bias.bw \
+                                                      --bed_file regions_test.bed --extend 0 --window 101 \
+                                                      --out_dir . --out_name ecoli_to_human_naked_dna_corrected \
+                                                      --chrom_size_file ../hg38.chrom.sizes  # ~5 min for chr21
