7dd934f8602750ffdbae57c124c03a67a7d2f1cb
Luz <779410241@qq.com>
2026-01-14 20:08:42 +0800

    Update PRINT_seq2print.py

--- TF_grammar/PRINT_seq2print.py
+++ TF_grammar/PRINT_seq2print.py
@@ -1,4 +1,4 @@
-########## train seq2print model ##########
+################################################################## train seq2print model #######################################################################################
 ## workdir: /fs/home/jiluzhang/TF_grammar/scPrinter/20260114/seq2print
 ## datasets.py (mask some code)
 ## env: scPrinter
@@ -21,12 +21,13 @@
 # files for seq2print model training: hg38_bias_v2.h5 gencode_v41_GRCh38.fa.gz
 
 CUDA_VISIBLE_DEVICES=7 python seq2print_lora_train.py --config ./config.JSON --temp_dir ./temp  --model_dir ./model \
+####################################################################################################################################################################################
 
-
-########## train TFBS model ##########
+################################################################################ train TFBS model ################################################################################
 ## workdir: /fs/home/jiluzhang/TF_grammar/scPrinter/20260114/tfbs
 # cp /fs/home/jiluzhang/TF_grammar/scPrinter/test/luz/Unibind/data/TFBSPrediction/HepG2_pred_data.tsv .
 # cp /fs/home/jiluzhang/TF_grammar/scPrinter/test/luz/Unibind/data/HepG2/test_peaks.bed .
+# cp /fs/home/jiluzhang/TF_grammar/scPrinter/20260114/seq2print/model/Bcell_0_fold0-hmxm767i.pt_deepshap_sample30000/*bigwig .
 #### TFBS_finetune.ipynb
 ## conda activate scPrinter
 # reload imported modules
@@ -51,6 +52,20 @@ import pandas as pd
 from concurrent.futures import *
 from tqdm.auto import *
 import pickle
+from pyfaidx import Fasta
+from scipy.stats import *
+from sklearn.model_selection import train_test_split
+import pickle
+import argparse
+import torch.nn.functional as F
+from sklearn.metrics import average_precision_score
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from ema_pytorch import EMA
+from sklearn.preprocessing import *
+from scipy.stats import pearsonr
 
 ## split range to chrom & start & end
 def read_TF_loci(path):
@@ -96,59 +111,11 @@ def get_normalization_factor(model, feat, peak, low=0.05, median = 0.5, high=0.9
     vs = np.nan_to_num(vs)
     return np.quantile(vs, low), np.quantile(vs, median), np.quantile(vs, high)
 
-# wget -c https://raw.githubusercontent.com/ruochiz/FigRmotifs/main/human_pfms_v4.txt
-motifs = scp.motifs.Motifs("./human_pfms_v4.txt", scp.genome.hg38.fetch_fa(), scp.genome.hg38.bg)
-motif2matrix = {motif.name.split("_")[2]: np.array([motif.counts['A'], motif.counts['C'], motif.counts['G'], motif.counts['T']]) for motif in motifs.all_motifs}
-
-## normalization to keep colsum of 1
-for m in motif2matrix:
-    mm = motif2matrix[m]
-    mm = mm / np.sum(mm, axis=0, keepdims=True)
-    motif2matrix[m] = mm
-
-tsv_paths = {'HepG2': "./HepG2_pred_data.tsv"}
-peaks_path = {'HepG2': "./test_peaks.bed"}
-
-tsvs = {cell:read_TF_loci(tsv_paths[cell]) for cell in tsv_paths}
-peaks = {cell:read_peaks(peaks_path[cell]) for cell in peaks_path}
-model_name = {'HepG2': ["./"]}
-
-
-feats = ['attr.count.shap_hypo_0_.0.85.bigwig', 'attr.just_sum.shap_hypo_0-30_.0.85.bigwig']  # bigwig generated after model training finishied
-
-pool= ProcessPoolExecutor(max_workers=3)
-norm_factor = {}
-p_list = []
-for cell in model_name:
-    norm_factor[cell] = {}
-    for feat in feats:
-        norm_factor[cell][feat] = []
-        for model in model_name[cell]:
-            p = pool.submit(get_normalization_factor, model, feat, peaks[cell], sample_num=-1)
-            norm_factor[cell][feat].append(p)
-            p_list.append(p)
-
-for cell in model_name:
-    for feat in feats:
-        for i, model in enumerate(model_name[cell]):
-            norm_factor[cell][feat][i] = norm_factor[cell][feat][i].result()
-
-for cell in model_name:
-    for feat in feats:
-        for i, model in enumerate(model_name[cell]):
-            lo, mid, hi = norm_factor[cell][feat][i]
-            print(cell, feat, i, "%.2f" % mid, "%.2f"%(hi-lo))
-
-from pyfaidx import Fasta
-from scipy.stats import *
-
 def cosine(x,y):
     return (x*y).sum() / (np.linalg.norm(x) * np.linalg.norm(y))
 
-def get_feats(model, feat, tsv,
-              flank,
-              low=None, median=None, high=None, chroms = None,
-             verbose=False):
+def get_feats(model, feat, tsv, flank, low=None, median=None, high=None, 
+              chroms=None, verbose=False):
     if low is None:
         low = 0
         median = 0
@@ -176,7 +143,6 @@ def get_feats(model, feat, tsv,
             if sd != '+':
                 v = v[::-1]
 
-            # seq = hg38.fetch_onehot_seq(c, start-1, end).numpy()
             seq = fasta[c][start-1:end].seq
             seq = scp.utils.DNA_one_hot(seq).numpy()
             seq_trend = f.values(c, start-1, end, numpy=True)
@@ -199,89 +165,17 @@ def get_feats(model, feat, tsv,
     signals = (signals - median) / (high - low)
     return signals, similarity, tsv
 
-
-feats_all = {}
-labels_all = {}
-pool= ProcessPoolExecutor(max_workers=3)
-p_list = []
-for cell in model_name:
-    for feat in feats:
-        for i, model in enumerate(model_name[cell]):
-            lo, mid, hi = None, None, None
-            p_list.append(pool.submit(get_feats, model, feat, tsvs[cell], 100, lo, mid, hi))
-
-ct = 0
-for cell in model_name:
-    feats_cell = []
-    sims_cell = []
-    for feat in feats:
-        feats_v = 0
-        sims_v = 0
-        for i, model in enumerate(model_name[cell]):
-            feats_, sims_, labels = p_list[ct].result()
-            ct += 1
-            feats_v += feats_
-            sims_v += sims_
-        feats_v = feats_v / len(model_name[cell])
-        sims_v = sims_v / len(model_name[cell])
-        feats_cell.append(feats_v)
-        sims_cell.append(sims_v)
-    feats_cell = np.stack(feats_cell, axis=1)
-    sims_cell = np.stack(sims_cell, axis=1)
-    feats_all[cell] = [feats_cell, sims_cell]
-    labels_all[cell] = labels
-
-pool.shutdown(wait=True)
-
-from sklearn.model_selection import train_test_split
-hepg2_train, hepg2_val = train_test_split(np.arange(len(labels_all['HepG2'])), test_size=0.1, random_state=42)
-gm_train, gm_val = train_test_split(np.arange(len(labels_all['HepG2'])), test_size=0.1, random_state=0)
-
-train_cells_hepg2 = ['HepG2']
-train_cells_gm12878 = ['HepG2']
-
 def feats_for_cell(feats_all, labels_all, cell, window=201, n_feats=np.arange(8)):
     seq_attr = feats_all[cell][0]
     pad = (seq_attr.shape[-1] - window) // 2
     if pad > 0:
         seq_attr = seq_attr[..., pad:-pad]
-    # seq_attr = seq_attr[:, n_feats].reshape((len(seq_attr), -1))
     seq_motif_sim = feats_all[cell][1][:, n_feats]
     motifs = np.asarray(labels_all[cell]['motifMatchScore'])[:, None]
     motifs = np.stack([motifs] * len(n_feats), axis=1)
-    # feats = seq_motif_sim.reshape((len(seq_attr), -1))
     feats = np.concatenate([motifs, seq_motif_sim, seq_attr], axis=-1)
     print (motifs.shape, seq_motif_sim.shape, seq_attr.shape)
     return feats
-
-train_feats = np.concatenate([feats_for_cell(feats_all, labels_all, cell,n_feats=np.arange(2))[hepg2_train] for cell in train_cells_hepg2] + 
-                             [feats_for_cell(feats_all, labels_all, cell,n_feats=np.arange(2))[gm_train] for cell in train_cells_gm12878], axis=0)
-valid_feats = np.concatenate([feats_for_cell(feats_all, labels_all, cell,n_feats=np.arange(2))[hepg2_val] for cell in train_cells_hepg2] + 
-                             [feats_for_cell(feats_all, labels_all, cell,n_feats=np.arange(2))[gm_val] for cell in train_cells_gm12878], axis=0)
-
-train_labels = np.concatenate([np.asarray(labels_all[cell]['bound'])[hepg2_train] for cell in train_cells_hepg2] + 
-                              [np.asarray(labels_all[cell]['bound'])[gm_train] for cell in train_cells_gm12878], axis=0)
-valid_labels = np.concatenate([np.asarray(labels_all[cell]['bound'])[hepg2_val] for cell in train_cells_hepg2] + 
-                              [np.asarray(labels_all[cell]['bound'])[gm_val] for cell in train_cells_gm12878], axis=0)
-
-import pickle
-pickle.dump([train_feats, valid_feats, train_labels, valid_labels], open("finetune_TFBS.pkl", "wb"))
-
-train_feats = np.concatenate([feats_for_cell(feats_all, labels_all, cell,n_feats=np.arange(2))[gm_train] for cell in train_cells_gm12878], axis=0)
-valid_feats = np.concatenate([feats_for_cell(feats_all, labels_all, cell,n_feats=np.arange(2))[gm_val] for cell in train_cells_gm12878], axis=0)
-pickle.dump([train_feats, valid_feats, train_labels, valid_labels], open("finetune_TFBS_GM.pkl", "wb"))
-
-train_labels = np.concatenate(
-[np.asarray(labels_all[cell]['bound'])[gm_train] for cell in train_cells_gm12878], axis=0)
-valid_labels = np.concatenate([np.asarray(labels_all[cell]['bound'])[gm_val] for cell in train_cells_gm12878], axis=0)
-
-import argparse
-import pickle
-
-import numpy as np
-import torch.nn.functional as F
-from sklearn.metrics import average_precision_score
-
 
 # Validation step with AUPR reporting
 def validate_model(model, val_loader, criterion, device="cpu"):
@@ -304,14 +198,6 @@ def validate_model(model, val_loader, criterion, device="cpu"):
     val_loss *= 100
     val_aupr = average_precision_score(all_labels, all_preds)  # Compute AUPR
     return val_loss, val_aupr
-
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-
-from ema_pytorch import EMA
 
 class FocalLoss(nn.Module):
     def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
@@ -336,8 +222,6 @@ class FocalLoss(nn.Module):
         else:
             return F_loss
 
-
-# 1. Define the MLP Model
 class MLP(nn.Module):
     def __init__(self, input_size, input_channel, mean, scale):
         super(MLP, self).__init__()
@@ -364,7 +248,6 @@ class MLP(nn.Module):
         xx = self.fc4(xx)  # No activation, will use nn.CrossEntropyLoss
         # xx = xx + self.fc5(x[..., :3].mean(dim=1))
         return xx
-
 
 class TFConv(nn.Module):
     def __init__(self, input_size, input_channel, mean, scale):
@@ -406,7 +289,6 @@ class TFConv(nn.Module):
         xx = self.dropout3(self.activation3(self.conv3(xx)))
         xx = self.conv4(xx)
         return xx
-
 
 class TFConv_translate(nn.Module):
     def __init__(self, tfconv):
@@ -460,10 +342,92 @@ class TFConv_translate(nn.Module):
         xx = self.conv4(xx)
         return xx
 
-torch.set_num_threads(3)
+# wget -c https://raw.githubusercontent.com/ruochiz/FigRmotifs/main/human_pfms_v4.txt
+motifs = scp.motifs.Motifs("./human_pfms_v4.txt", scp.genome.hg38.fetch_fa(), scp.genome.hg38.bg)
+motif2matrix = {motif.name.split("_")[2]: np.array([motif.counts['A'], motif.counts['C'], motif.counts['G'], motif.counts['T']]) for motif in motifs.all_motifs}
 
-from sklearn.preprocessing import *
-from scipy.stats import pearsonr
+## normalization to keep colsum of 1
+for m in motif2matrix:
+    mm = motif2matrix[m]
+    mm = mm / np.sum(mm, axis=0, keepdims=True)
+    motif2matrix[m] = mm
+
+tsv_paths = {'HepG2': "./HepG2_pred_data.tsv"}
+peaks_path = {'HepG2': "./test_peaks.bed"}
+
+tsvs = {cell:read_TF_loci(tsv_paths[cell]) for cell in tsv_paths}
+peaks = {cell:read_peaks(peaks_path[cell]) for cell in peaks_path}
+model_name = {'HepG2': ["./"]}
+
+feats = ['attr.count.shap_hypo_0_.0.85.bigwig', 'attr.just_sum.shap_hypo_0-30_.0.85.bigwig']  # bigwig generated after seq2print model training finishied
+
+pool= ProcessPoolExecutor(max_workers=2)
+norm_factor = {}
+p_list = []
+for cell in model_name:
+    norm_factor[cell] = {}
+    for feat in feats:
+        norm_factor[cell][feat] = []
+        for model in model_name[cell]:
+            p = pool.submit(get_normalization_factor, model, feat, peaks[cell], sample_num=-1)
+            norm_factor[cell][feat].append(p)
+            p_list.append(p)
+
+for cell in model_name:
+    for feat in feats:
+        for i, model in enumerate(model_name[cell]):
+            norm_factor[cell][feat][i] = norm_factor[cell][feat][i].result()
+            lo, mid, hi = norm_factor[cell][feat][i]
+            print(cell, feat, i, "%.2f" % mid, "%.2f"%(hi-lo))
+
+feats_all = {}
+labels_all = {}
+p_list = []
+for cell in model_name:
+    for feat in feats:
+        for i, model in enumerate(model_name[cell]):
+            lo, mid, hi = None, None, None
+            p_list.append(pool.submit(get_feats, model, feat, tsvs[cell], 100, lo, mid, hi))
+
+ct = 0
+for cell in model_name:
+    feats_cell = []
+    sims_cell = []
+    for feat in feats:
+        feats_v = 0
+        sims_v = 0
+        for i, model in enumerate(model_name[cell]):
+            feats_, sims_, labels = p_list[ct].result()
+            ct += 1
+            feats_v += feats_
+            sims_v += sims_
+        feats_v = feats_v / len(model_name[cell])
+        sims_v = sims_v / len(model_name[cell])
+        feats_cell.append(feats_v)
+        sims_cell.append(sims_v)
+    feats_cell = np.stack(feats_cell, axis=1)
+    sims_cell = np.stack(sims_cell, axis=1)
+    feats_all[cell] = [feats_cell, sims_cell]
+    labels_all[cell] = labels
+
+pool.shutdown(wait=True)
+
+hepg2_train, hepg2_val = train_test_split(np.arange(len(labels_all['HepG2'])), test_size=0.1, random_state=42)
+gm_train, gm_val = train_test_split(np.arange(len(labels_all['HepG2'])), test_size=0.1, random_state=0)
+
+train_cells_hepg2 = ['HepG2']
+train_cells_gm12878 = ['HepG2']
+train_feats = np.concatenate([feats_for_cell(feats_all, labels_all, cell, n_feats=np.arange(2))[hepg2_train] for cell in train_cells_hepg2] + 
+                             [feats_for_cell(feats_all, labels_all, cell, n_feats=np.arange(2))[gm_train] for cell in train_cells_gm12878], axis=0)
+valid_feats = np.concatenate([feats_for_cell(feats_all, labels_all, cell, n_feats=np.arange(2))[hepg2_val] for cell in train_cells_hepg2] + 
+                             [feats_for_cell(feats_all, labels_all, cell, n_feats=np.arange(2))[gm_val] for cell in train_cells_gm12878], axis=0)
+train_labels = np.concatenate([np.asarray(labels_all[cell]['bound'])[hepg2_train] for cell in train_cells_hepg2] + 
+                              [np.asarray(labels_all[cell]['bound'])[gm_train] for cell in train_cells_gm12878], axis=0)
+valid_labels = np.concatenate([np.asarray(labels_all[cell]['bound'])[hepg2_val] for cell in train_cells_hepg2] + 
+                              [np.asarray(labels_all[cell]['bound'])[gm_val] for cell in train_cells_gm12878], axis=0)
+pickle.dump([train_feats, valid_feats, train_labels, valid_labels], open("finetune_TFBS.pkl", "wb"))
+
+torch.set_num_threads(3)
 
 for feats in [[0], [1]]:
     feats = np.array(feats)
@@ -489,8 +453,8 @@ for feats in [[0], [1]]:
     class_counts = y_train.sum()
     class_counts = torch.as_tensor([(class_counts) / len(y_train)])
     class_weights = 1.0 / class_counts.float()
-    # Create DataLoader instances
     
+    # Create DataLoader instances
     train_dataset = TensorDataset(X_train, y_train)
     val_dataset = TensorDataset(X_val, y_val)
     
@@ -503,11 +467,10 @@ for feats in [[0], [1]]:
     ema = EMA(model, beta=0.9999,  # exponential moving average factor
               update_after_step=100,  # only after this number of .update() calls will it start updating
               update_every=1).cuda()
-    # 3. Training Loop
+    
+    # Training Loop
     criterion = nn.BCEWithLogitsLoss(weight=class_weights.cuda())
-    
     optimizer = optim.Adam(model.parameters(), lr=5e-4)  # Learning rate can be adjusted
-    
     num_epochs = 1000  # Number of epochs can be adjusted
     val_freq = 1000
     best_val_loss = 0
@@ -529,7 +492,7 @@ for feats in [[0], [1]]:
                 ct = 0
                 break
     
-        # 4. Validation Step
+        # Validation Step
         m = ema.ema_model
         val_loss, val_aupr = validate_model(m, val_loader, criterion, "cuda")
         print(f"Epoch: {epoch + 1}, Validation Loss: {val_loss:.4f}, Validation AUPR: {val_aupr:.4f}")
@@ -551,8 +514,10 @@ for feats in [[0], [1]]:
     print(a.shape, b.shape, a.reshape((-1)), b.reshape((-1)), torch.allclose(a, b, atol=1e-3, rtol=1e-3))
     print(pearsonr(a.reshape((-1)).cpu().detach().numpy(), b.reshape((-1)).cpu().detach().numpy()))
     m2 = torch.jit.script(m2)
+    
     # Save to file
     torch.jit.save(m2, f"TFBS_{feats[0]}_conv_v2.pt")
+##########################################################################################################################################################################
 
 
 
